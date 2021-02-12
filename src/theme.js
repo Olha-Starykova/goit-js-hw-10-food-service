13c61f9bd4f60c64ccc6d6294e3fe9eebf6ffab6
Olha-Starykova <olenkalog@gmail.com>
2021-02-12 21:10:15 +0200

    com

--- src/theme.js
+++ src/theme.js
@@ -9,38 +9,59 @@ const refs = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+//вариант первый ставим класс на боди  <div class="theme-switch"> 
+refs.checkBox.addEventListener('click', toogleTeme);
 
-refs.checkBox.addEventListener('change', setClassList);
-refs.checkBox.addEventListener('change', setLocalStorage);
-
-function setClassList() {
-    const check = refs.checkBox.checked;
- if (check) {
-    refs.body.classList.add(Theme.DARK);
-    refs.body.classList.remove(Theme.LIGHT);
-  } else {
-    refs.body.classList.add(Theme.LIGHT);
-    refs.body.classList.remove(Theme.DARK);
-  }
-};
-
-function setLocalStorage() {
-  const check = refs.checkBox.checked;
-
-  if (check) {
-    localStorage.setItem('theme', Theme.DARK);
-  } else {
-    localStorage.removeItem('theme');
-    localStorage.setItem('theme', Theme.LIGHT);
-  }
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  themeName === Theme.DARK
+    ? refs.body.classList.replace(Theme.LIGHT, Theme.DARK)
+    : refs.body.classList.replace(Theme.DARK, Theme.LIGHT)
 }
 
-const themeHistory = localStorage.getItem('theme');
+function toogleTeme() {
+  refs.checkBox.checked
+    ? setTheme(Theme.DARK)
+    : setTheme(Theme.LIGHT);
+}
 
-if (themeHistory === Theme.DARK) {
-  refs.body.classList.add(Theme.DARK);
-  refs.checkBox.checked = true;
-};
+if (localStorage.getItem('theme') === Theme.DARK) {
+  refs.checkBox.setAttribute('checked', 'true');
+  toogleTeme();
+}
+//вариант второй, класс на боди не нужен
+
+// refs.checkBox.addEventListener('change', setClassList);
+// refs.checkBox.addEventListener('change', setLocalStorage);
+
+// function setClassList() {
+//     const check = refs.checkBox.checked;
+//  if (check) {
+//     refs.body.classList.add(Theme.DARK);
+//     refs.body.classList.remove(Theme.LIGHT);
+//   } else {
+//     refs.body.classList.add(Theme.LIGHT);
+//     refs.body.classList.remove(Theme.DARK);
+//   }
+// };
+
+// function setLocalStorage() {
+//   const check = refs.checkBox.checked;
+
+//   if (check) {
+//     localStorage.setItem('theme', Theme.DARK);
+//   } else {
+//     localStorage.removeItem('theme');
+//     localStorage.setItem('theme', Theme.LIGHT);
+//   }
+// }
+
+// const themeHistory = localStorage.getItem('theme');
+
+// if (themeHistory === Theme.DARK) {
+//   refs.body.classList.add(Theme.DARK);
+//   refs.checkBox.checked = true;
+// };
 
 
 
